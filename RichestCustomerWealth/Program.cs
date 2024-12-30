9bbec7d05b088b965325c027b56cf4c296f98b95
Admin <Admin@DESKTOP-DFAIJ8K>
2024-12-30 11:52:12 +0200

    Add project files.

---
+++ RichestCustomerWealth/Program.cs
@@ -0,0 +1,82 @@
+﻿//1672. Richest Customer Wealth
+//(1672. Статки найбагатших клієнтів)
+
+//You are given an m x n integer grid accounts where accounts[i][j]
+//is the amount of money the i​​​​​​​​​​​th​​​​ customer has in the j​​​​​​​​​​​th​​​​ bank. Return
+//the wealth that the richest customer has.
+//(Вам задано сітку рахунків розміром m x n цілих чисел, де
+//accounts[i][j] - кількість грошей, яку має i-й клієнт у j-му
+//банку. Виведіть суму, яку має найбагатший клієнт.)
+
+//A customer's wealth is the amount of money they have in all their
+//bank accounts. The richest customer is the customer that has the
+//maximum wealth.
+//(Статок клієнта - це сума грошей на всіх його банківських рахунках.
+//Найбагатший клієнт - це клієнт, який має максимальне багатство.)
+
+//new Solution().MaximumWealth([[1, 2, 3], [3, 2, 1]]);
+//public class Solution 
+//{
+//    public int MaximumWealth(int[][] accounts) 
+//    {
+//        return accounts.Select(account => account.Sum()).Max();
+//    }
+//}
+
+//new Solution().MaximumWealth([[1, 2, 3], [3, 2, 1]]);
+//public class Solution
+//{
+//	public int MaximumWealth(int[][] accounts)
+//	{
+//		return accounts.Select(account => account.Sum()).Max();
+//	}
+//}
+
+//new Solution().MaximumWealth([[1, 2, 3], [3, 2, 1]]);
+//public class Solution
+//{
+//	public int MaximumWealth(int[][] accounts)
+//	{
+//		return accounts.Select(account => account.Sum()).Max();
+//	}
+//}
+
+//new Solution().MaximumWealth([[1, 2, 3], [3, 2, 1]]);
+//public class Solution
+//{
+//	public int MaximumWealth(int[][] accounts)
+//	{
+//		return accounts.Select(account => account.Sum()).Max();
+//	}
+//}
+
+//new Solution().MaximumWealth([[1, 2, 3],[3, 2, 1]]);
+//public class Solution
+//{
+//	public int MaximumWealth(int[][] accounts)
+//	{
+//		return accounts.Select(account => account.Sum()).Max(); 
+//	}
+//}
+
+//using System.Net.Sockets;
+
+//new Solution().MaximumWealth([[1, 2, 3], [3, 2, 1]]);
+//public class Solution
+//{
+//	public int MaximumWealth(int[][] accounts) 
+//	{
+//		return accounts.Select(account => account.Sum()).Max();
+//	}
+//}
+
+using System.Security.Cryptography.X509Certificates;
+
+new Solution().MaximumWealth([[1, 2, 3], [3, 2, 1]]);
+public class Solution()
+{
+	public int MaximumWealth(int[][] accounts)
+	{
+		return accounts.Select(account => account.Sum()).Max();
+	}
+}
